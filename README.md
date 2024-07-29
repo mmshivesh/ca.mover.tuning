32763b74acc1498c4f66158fa2ec8403edf084a9
R3yn4ld <reynald.rollet@gmail.com>
2024-07-29 02:18:14 +0200

    2024-07-29
    - Complete rewrite of file listing functions (find, decide to move..) (R3yn4ld)
    - Internal moving/syncing engine
    - Updated zfs functions getting usage of a pool (R3yn4ld, greetings to Freender on forums for pointing out and proposal)
    - Added cache mode "prefer" smart moving in "Automatic age" mode (R3yn4ld)
    - Added option to "repair" Cache:Only (moving everything on share to cache) and Cache:No (moving everything on share to array) shares (R3yn4ld)
    - Added option to synchronize Cache:Yes and Cache:Prefer shares to array so data are parity protected (R3yn4ld)
    - Turbo write mode forcing improvement to not wake spinners if not needed (R3yn4ld)
    - UI improvements (R3yn4ld)

--- README.md
+++ README.md
@@ -10,9 +10,34 @@ This is a simple [Unraid](https://unraid.net/) plugin that will let you fine-tun
 - Optional ability to completely disable the scheduled runs of mover
 - Manually executed runs of mover ("Move Now" button) can either follow the rules for schedules, or always move all files
 
-This repo merge all [pull requests](https://github.com/hugenbd/ca.mover.tuning/pulls) after review from [Hugenbd's repo](https://github.com/hugenbd/ca.mover.tuning) (cosmetics, merge skipfiletypes from shares, 4 changes from Swarles below) and add several feature, as for example automatic age threshold and compatibility with Unraid 7.x, and other stuff coming.
+This new fork merge all [pull requests](https://github.com/hugenbd/ca.mover.tuning/pulls) after review from [Hugenbd's repo](https://github.com/hugenbd/ca.mover.tuning) (cosmetics, merge skipfiletypes from shares, 4 changes from Swarles below) and add several feature, as for example automatic age threshold and compatibility with Unraid 7.x, and other stuff coming.
+
+## How it works:
+
+First it checks if it's valid for this script run: there must be a cache disk present and an instance of the script must not already be running.
+
+Next, check each of the top-level directories (shares) on the cache disk.
+For all share with 'Use Cache' setting set to "prefer" or "yes", we use 'find' to create a filtered file list of that share directory.
+For all share with 'Use Cache' setting set to "only", we use 'du' or 'zfs list' to get total size of that share directory.
+
+The list is sorted by "Use cache", increasing age, pool, and file inode, giving priority for being on cache to "cache only" shares, then "cache prefer" by moving newest from array to cache and older to array, and finally to "cache yes" share by moving only from cache to array.
+Please note that if age setting is set to something else than "Auto (smart cache)" this script is actually dumb and do not check for size and free space and rely on your own calculations.
+Files at the top level of the cache or an array disk (i.e not in a share) are never moved.
+
+The list is then passed to original unraid mover.
+For each file, if the file is not "in use" by any process (as detected by 'fuser' command), then the file is moved, and upon success, deleted from the source disk.  If the file already exists on the target, it is not moved and the sourceis not deleted.  All meta-data of moved files/directories is preserved: permissions, ownership, extended attributes, and access/modified timestamps.
+If an error occurs in copying a file, the partial file, if present, is deleted and the operation continues on to the next file.
 
 ## Changelog
+- 2024-07-29
+    - Complete rewrite of file listing functions (find, decide to move..) (R3yn4ld)
+    - Updated zfs functions getting usage of a pool (R3yn4ld, greetings to ([Freender on forums](https://forums.unraid.net/topic/70783-plugin-mover-tuning/page/61/#comment-1442327)) for pointing out and proposal)
+    - Added cache mode "prefer" smart moving in "Automatic age" mode (R3yn4ld)
+    - Added option to "repair" Cache:Only (moving everything on share to cache) and Cache:No (moving everything on share to array) shares (R3yn4ld)
+    - Added option to synchronize Cache:Yes and Cache:Prefer shares to array so data are parity protected (R3yn4ld)
+    - Turbo write mode forcing improvement to not wake spinners if not needed (R3yn4ld)
+    - UI improvements (R3yn4ld)
+
 - 2024-07-10
     - [Unraid 7.0.0 compatibility](https://github.com/R3yn4ld/ca.mover.tuning/tree/unraid-7.0.0-compatibility) ([R3yn4ld](https://github.com/R3yn4ld)): original mover now works with "Move Now button follows plug-in filters" set to off - ([R3yn4ld](https://github.com/R3yn4ld))
 
--- source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/mover.php
+++ source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/mover.php
@@ -35,10 +35,6 @@ function startMover($options = "start")
             exit();
         }
     }
-    if ($cfg['enableTurbo'] == "yes") {
-        logger("Forcing turbo write on");
-        exec("/usr/local/sbin/mdcmd set md_write_method 1");
-    }
 
     if ($options == "stop") {
         $niceLevel = $cfg['moverNice'] ?: "0";
@@ -48,96 +44,17 @@ function startMover($options = "start")
         exit();
     }
 
-
     if ($cron or $cfg['movenow'] == "yes") {
         //exec("echo 'running from cron or move now question is yes' >> /var/log/syslog");
-        $beforescript = $cfg['beforeScript'];
-        $afterscript = $cfg['afterScript'];
+        $niceLevel = $cfg['moverNice'] ?: "0";
+        $ioLevel = $cfg['moverIO'] ?: "-c 2 -n 0";
 
         if ($cfg['threshold'] >= 0 or $cfg['age'] == "yes" or $cfg['sizef'] == "yes" or $cfg['sparsnessf'] == "yes" or $cfg['filelistf'] == "yes" or $cfg['filetypesf'] == "yes" or $beforescript != '' or $afterscript != '' or $cfg['testmode'] == "yes") {
-
 			$age_mover_str = "/usr/local/emhttp/plugins/ca.mover.tuning/age_mover start";
-            $niceLevel = $cfg['moverNice'] ?: "0";
-            $ioLevel = $cfg['moverIO'] ?: "-c 2 -n 0";
-            $threshold = $cfg['threshold'];
-            $ageLevel = $cfg['daysold'];
-            $sizeLevel = $cfg['sizeinM'];
-            $sparsnessLevel = $cfg['sparsnessv'];
-            $filelistLevel = $cfg['filelistv'];
-            $filetypesLevel = $cfg['filetypesv'];
-            $ctime = $cfg['ctime'];
-            $testmode = $cfg['testmode']; #Not used
-            $omoverth = $cfg['omoverthresh'];
-            $ihidden = $cfg['ignoreHidden']; #Not used
-
-            #build age_mover command for all options.
-            if ($cfg['age'] == "yes") {
-                $age_mover_str = "$age_mover_str $ageLevel";
-            } else {
-                $age_mover_str = "$age_mover_str 0";
-            }
-            if ($cfg['sizef'] == "yes") {
-                $age_mover_str = "$age_mover_str $sizeLevel";
-            } else {
-                $age_mover_str = "$age_mover_str 0";
-            }
-            if ($cfg['sparsnessf'] == "yes") {
-                $age_mover_str = "$age_mover_str $sparsnessLevel";
-            } else {
-                $age_mover_str = "$age_mover_str 0";
-            }
-            if ($cfg['filelistf'] == "yes") {
-                $age_mover_str = "$age_mover_str \"$filelistLevel\"";
-            } else {
-                $age_mover_str = "$age_mover_str ''";
-            }
-            if ($cfg['filetypesf'] == "yes") {
-                $age_mover_str = "$age_mover_str \"$filetypesLevel\"";
-            } else {
-                $age_mover_str = "$age_mover_str ''";
-            }
-            if (empty($beforescript)) {
-                $age_mover_str = "$age_mover_str ''";
-            } else {
-                $age_mover_str = "$age_mover_str \"$beforescript\"";
-            }
-            if (empty($afterscript)) {
-                $age_mover_str = "$age_mover_str ''";
-            } else {
-                $age_mover_str = "$age_mover_str \"$afterscript\"";
-            }
-            if (empty($ctime)) {
-                $age_mover_str = "$age_mover_str ''";
-            } else {
-                $age_mover_str = "$age_mover_str $ctime";
-            }
-            if ($cfg['omovercfg'] == "yes") {
-                $age_mover_str = "$age_mover_str $omoverth";
-            } else {
-                $age_mover_str = "$age_mover_str ''";
-            }
-            if ($cfg['testmode'] == "yes") {
-                $age_mover_str = "$age_mover_str 'yes'";
-            } else {
-                $age_mover_str = "$age_mover_str ''";
-            }
-            if ($cfg['ignoreHidden'] == "yes") {
-                $age_mover_str = "$age_mover_str 'yes'";
-            } else {
-                $age_mover_str = "$age_mover_str ''";
-            }
-            if (empty($threshold)) {
-                $age_mover_str = "$age_mover_str 0";
-            } else {
-                $age_mover_str = "$age_mover_str $threshold";
-            }
-
             //exec("echo 'about to hit mover string here: $age_mover_str' >> /var/log/syslog");
-
             logger("ionice $ioLevel nice -n $niceLevel $age_mover_str");
             passthru("ionice $ioLevel nice -n $niceLevel $age_mover_str");
         }
-
     } else {
         //exec("echo 'Running from button' >> /var/log/syslog");
         //Default "move now" button has been hit.
@@ -145,13 +62,6 @@ function startMover($options = "start")
         $ioLevel = $cfg['moverIO'] ?: "-c 2 -n 0";
         logger("ionice $ioLevel nice -n $niceLevel /usr/local/sbin/mover.old $options");
         passthru("ionice $ioLevel nice -n $niceLevel /usr/local/sbin/mover.old $options");
-
-    }
-
-
-    if ($cfg['enableTurbo'] == "yes") {
-        logger("Restoring original turbo write mode");
-        exec("/usr/local/sbin/mdcmd set md_write_method {$vars['md_write_method']}");
     }
 
 }
